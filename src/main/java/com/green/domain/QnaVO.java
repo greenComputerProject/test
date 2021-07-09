b6aa8f43dffc68af438d0947256ffa981cee0d41
Songminseob <songminseob@naver.com>
2021-07-09 09:42:47 +0900

    Qna_1

--- src/main/java/com/green/domain/QnaVO.java
+++ src/main/java/com/green/domain/QnaVO.java
@@ -1,26 +1,15 @@
 package com.green.domain;
 
-<<<<<<< HEAD
-=======
-import java.util.Date;
 
->>>>>>> 65ae237eef8848fd28ec9e51a5d26f85d5631d53
+
 import lombok.Data;
 
 @Data
 public class QnaVO {
-<<<<<<< HEAD
 	private Long qno_no;
 	private String qna_title;
 	private String qna_content;
 	private Data qna_regDate;
 	private String userid;
-=======
-	//질문
-	private Long qna_no;
-    private String qna_title;
-    private String qna_content;
-    private Date qna_regDate;
-    private String userid;
->>>>>>> 65ae237eef8848fd28ec9e51a5d26f85d5631d53
+
 }
